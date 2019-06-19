b22ba5f2aac8ff678552627c6627484daf7a525c
omar20alaa <omar-p@invetechs.com>
2019-06-19 08:46:45 +0200

    push project

---
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -0,0 +1,12 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+
+    @Override
+    protected String getMainComponentName() {
+        return "AwesomeProject";
+    }
+}
